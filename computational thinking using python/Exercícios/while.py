13034ada412ce537d97a8a79edd99ab60a985c15
Guilherme Rocha <guilherme.rtds1405@gmail.com>
2023-04-11 19:47:34 -0300

    add

--- computational thinking using python/Exercícios/while.py
+++ computational thinking using python/Exercícios/while.py
@@ -22,11 +22,22 @@ print("A soma dos números inteiros positivos de 1 a 100 é {}." .format(soma))
 contador = 1
 soma_while = 0
 
- while contador <= 100:
+while contador <= 100:
       soma_while += contador
       
 print("A soma dos números inteiros positivos de 1 a {} é {}." .format(contador, soma_while))
 
+# imprima a tabuada do número 5, de 0 a 10, inclusive
+# depois, adapte o programa para receber dois números do usuário: o de início e o de fim, utilize o "for" e "while"
+
+n1 = float(input("Diga um número inteiro para o início da tabuada:"))
+n2 = float(input("Diga um número inteiro para o final da tabuada:"))
+
+while n1!=int(n1):
+    n1 = float(input("Diga um número inteiro para o início da tabuada:"))
+
+while True:
+      n1 = float(input("Diga um número inteiro para o início da tabuada:"))
 
 
 
